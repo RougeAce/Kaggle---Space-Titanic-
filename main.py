e8b300e1ef1c3cd97afa0a67d43dd2b89d8097b9
Trey Davidson <treydavidson05@icloud.com>
2023-03-11 20:10:10 -0600

    Test 2

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+train_file = "spaceship-titanic/train.csv"
+train = pd.read_csv(train_file)
+
+# Drop rows with missing values
+train = train.dropna(how='any')
+
+# Group by HomePlanet and sum the Survived column for each group
+survivors = train.groupby('VIP')['Transported'].sum()
+
+# Create a pie chart
+plt.pie(survivors, labels=survivors.index, autopct='%1.1f%%')
+plt.title('Survivors by Planet')
+plt.show()
+
